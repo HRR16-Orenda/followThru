f6a11b0f2f03d752504dd2e8160f00f1aeac9176
S.B. Lee <echo3042@gmail.com>
2016-06-16 22:18:01 -0700

    (chore) check env variable of AWS

--- server/app.js
+++ server/app.js
@@ -4,6 +4,10 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
+  console.log(process.env.PORT);
+  console.log(process.env.RDS_PORT);
+  console.log(process.env.RDS_USERNAME);
+
   res.send('Hello world');
 });
 
