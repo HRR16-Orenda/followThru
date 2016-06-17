9215b88f9dd1feae293dd2c2519dfd01202c8bb3
Andrew Vye <a.vye26@gmail.com>
2016-06-17 15:44:34 -0700

    (feat) Update style sheet to remove center justification

--- client-ios/components/coreLayout.js
+++ client-ios/components/coreLayout.js
@@ -21,25 +21,10 @@ export default class Orenda extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <TouchableHighlight style={styles.button}>
-          <Text>
-            touch it!
-          </Text>
-        </TouchableHighlight>
         <TextInput
           style={{height: 40, borderColor: 'gray', borderWidth: 1}}
           //value={'Hey SB!'}
         />
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
         <Footer />
       </View>
     );
--- client-ios/styles/styles.js
+++ client-ios/styles/styles.js
@@ -5,8 +5,6 @@ import {
 export default StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
