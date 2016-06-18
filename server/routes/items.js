d9024fee532fcbb942388924744fe8eea42e14a0
S.B. Lee <echo3042@gmail.com>
2016-06-18 15:34:24 -0700

    (Feat&Test) write get handler for /items url and testing code

--- server/routes/items.js
+++ server/routes/items.js
@@ -1,10 +1,32 @@
+// @flow
 var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-  res.send('You are sending GET request to /items');
-});
+  // mock data for testing
+  var mockData = [
+    {
+      id: 1,
+      user: {
+        id: 1,
+        username: 'brent'
+      },
+      category: 'books',
+      subCategory: 'favorite',
+      title: 'The Lord of the Ring',
+      completed: false,
+      url: null,
+      createdAt: Date.now(),
+      recommendedBy: {
+        id: 2,
+        username: 'sb'
+      }
+    }
+  ];
 
+  // Should have interaction with DB controller to get whole items
+  res.send(mockData);
+});
 
 
 module.exports = router;
--- test/integration/ApiSpec.js
+++ test/integration/ApiSpec.js
@@ -1,6 +1,7 @@
 var expect = require('chai').expect;
 var app = require('../../server/app.js');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 4000;
+var supertest = require('supertest')(app);
 
 
 describe('API Test', function () {
@@ -18,5 +19,50 @@ describe('API Test', function () {
 
   it('should pass', function () {
     expect(true).to.equal(true);
+  });
+
+  describe('Route /items', function () {
+    describe('GET request', function () {
+      it('should return status code 200', function (done) {
+        supertest.get('/items')
+        .end(function (err, res) {
+          if(err) {return done(err)}
+          expect(res.status).to.equal(200);
+          done();
+        });
+      });
+
+      it('should return array with json type', function (done) {
+        supertest.get('/items')
+        .set('Accept', 'application/json')
+        .end(function (err, res) {
+          if(err) {return done(err)}
+          expect(res.body).to.be.an('array');
+          expect(res.body[0]).to.have.property('user');
+          expect(res.body[0].user.username).to.be.ok;
+          expect(res.body[0]).to.have.property('category');
+          expect(res.body[0]).to.have.property('subCategory');
+          expect(res.body[0]).to.have.property('title');
+          expect(res.body[0]).to.have.property('completed');
+          expect(res.body[0]).to.have.property('url');
+          expect(res.body[0]).to.have.property('createdAt');
+          expect(res.body[0]).to.have.property('recommendedBy');
+          done();
+        });
+      });
+    });
+  });
+
+  describe('Route /users', function () {
+    describe('GET request', function () {
+      it('should return status code 200', function (done) {
+        supertest.get('/users')
+        .end(function (err, res) {
+          if(err) {return done(err)}
+          expect(res.status).to.equal(200);
+          done();
+        });
+      });
+    });
   });
 });
