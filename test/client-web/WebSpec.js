c1f00380c74595b7c99c53017a442513036a4167
S.B. Lee <echo3042@gmail.com>
2016-06-23 00:37:57 -0700

    (test) setup client-web test

---
+++ test/client-web/WebSpec.js
@@ -0,0 +1,11 @@
+'use strict';
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount, render } from 'enzyme';
+
+describe('Test', function () {
+  it('should pass', function () {
+    const a = 1;
+    expect(a).to.equal(1);
+  });
+});
