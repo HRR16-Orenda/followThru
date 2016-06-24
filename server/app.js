80e1e4d48a426590f46e9d824d650c8547eb26ae
S.B. Lee <echo3042@gmail.com>
2016-06-23 20:25:27 -0700

    (feat) added cors module and morgan module for dev

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors');
 var bodyParser = require('body-parser');
 var sequelize = require('./db/config.js');
 var index = require('./routes/index.js');
@@ -8,7 +9,14 @@ var path = require('path');
 
 var app = express();
 
+if(process.env.NODE_ENV !== 'production') {
+  var morgan = require('morgan');
+  app.use(morgan('dev'));
+}
+
+
 // Middleware
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/', express.static(path.resolve(__dirname, '../build')));
