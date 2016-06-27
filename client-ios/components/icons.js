24f5a172c9a1873da892ca3c07258b063511b3c5
S.B. Lee <echo3042@gmail.com>
2016-06-26 21:01:40 -0700

    (feat) added Icon for tabBar

--- client-ios/components/icons.js
+++ client-ios/components/icons.js
@@ -1,13 +1,25 @@
 // @flow
 import React, { Component } from "react";
+import Icon from 'react-native-vector-icons/Ionicons';
 import {
+  View,
   Text
 } from 'react-native';
 
+const icons = {
+  Add: 'ios-create-outline',
+  List: 'ios-albums-outline',
+  Setting: 'ios-settings-outline'
+}
+
 export default class TabIcon extends React.Component {
   render(){
+    // Set default name for Icon
+    let name = icons[this.props.title] || 'ios-create-outline';
     return (
-      <Text style={{color: this.props.selected ? 'red' :'black'}}>{this.props.title}</Text>
+      <View>
+        <Icon name={name} size={30} color={this.props.selected ? 'blue' :'black'}/>
+      </View>
     );
   }
 }
