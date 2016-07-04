b793107e2b7ed2f5cf3777ed048f839783156540
Andrew Vye <a.vye26@gmail.com>
2016-07-04 15:45:00 -0700

    (feat) Add mobile unittest to the testing script

--- test/client-ios/component/ComponentSpec.js
+++ test/client-ios/component/ComponentSpec.js
@@ -2,25 +2,36 @@ import React, { View, Text, StyleSheet } from 'react-native';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
 
-// import * as actions from '../../client-ios/actions/index';
-// import * as types from '../../client-ios/constants/ActionTypes';
+import AddScreen from '../../../client-ios/components/addScreen';
+import Form from '../../../client-ios/components/form';
+import mainButtons from '../../../client-ios/components/mainButtons';
+import SingleListScreen from '../../../client-ios/components/singleListScreen';
 
-import ErrorMsgComponent from '../../../client-ios/components/errorMsg';
-// import FormComponent from '../../client-ios/components/form';
 
-describe('<ErrorMsgComponent />', () => {
-  it('should render stuff', () => {
-    const wrapper = shallow(<ErrorMsgComponent />);
-    console.log('****** the wrapper ', wrapper);
-    expect(wrapper.length).to.equal(1);
-    // expect(wrapper.contains(<Text>I wonder if there will be any problems...</Text>)).to.equal(true);
+
+
+// Problem with using redux is the wrapper is looking for Connect(Form) rather than just Form so it won't find it
+
+
+
+describe('<AddScreen />', () => {
+  it('should render the form and main buttons', () => {
+    const wrapper = shallow(<AddScreen />);
+    // expect(wrapper.length).to.equal(1);
+    console.log('***** looking for the view**** ', wrapper.find(View))
+    console.log('***** looking for the form**** ', wrapper.find(Form))
+    console.log('***** looking for the children**** ', wrapper.children())
+
+
+    expect(wrapper.find(View)).to.have.length(1);
+    // expect(wrapper.find(mainButtons)).to.have.length(1);
+    // expect(wrapper.contains(<Form />)).to.equal(true);
   });
 });
 
-// describe('<FormComponent />', () => {
+// describe('<SingleListScreen />', () => {
 //   it('should render stuff', () => {
-//     const wrapper = shallow(<FormComponent />);
-//     console.log('****** the wrapper ', wrapper);
+//     const wrapper = shallow(<ErrorMsgComponent />);
 //     expect(wrapper.length).to.equal(1);
 //     // expect(wrapper.contains(<Text>I wonder if there will be any problems...</Text>)).to.equal(true);
 //   });
