bf0ab1324d26d4213ab1233a5ab3b3586b89d447
S.B. Lee <echo3042@gmail.com>
2016-07-03 17:38:50 -0700

    (feat) wrote schema for followers table and set relationship between user table

--- server/models/followers.js
+++ server/models/followers.js
@@ -1,16 +1,8 @@
-//
-// var Sequelize = require('sequelize');
-// var sequelize = require('../db/config.js');
-//
-// var Follower = sequelize.define('follower', {
-//   accepted: {
-//     type: Sequelize.BOOLEAN,
-//     defaultValue: false,
-//     allowNull: false
-//   }
-// }, {underscored: true});
-//
-// //var Friend = sequelize.define('friend', {}, {undescored: true});
-// //Follower.hasMany(Follower, {as: 'follower'}); //this isn't right either
-// Follower.sync();
-// module.exports = Follower;
+var Sequelize = require('sequelize');
+var sequelize = require('../db/config.js');
+
+var Follower = sequelize.define('follower', {
+
+}, {underscored: true});
+
+module.exports = Follower;
