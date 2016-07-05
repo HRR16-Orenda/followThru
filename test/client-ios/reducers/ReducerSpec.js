e9faafdbabaff96a053aa9ae71f6717c2c8f8943
Andrew Vye <a.vye26@gmail.com>
2016-07-04 22:09:19 -0700

    (feat) Add more tests to the reducerSpec

--- test/client-ios/reducers/ReducerSpec.js
+++ test/client-ios/reducers/ReducerSpec.js
@@ -10,7 +10,7 @@ import * as types from '../../../client-ios/constants/ActionTypes';
 describe('Reducers', () => {
 
   describe('authReducer', () => {
-    it('should handle an login success', () => {
+    it('should handle a login success', () => {
       const action = {
         type: 'LOGIN_SUCCESS',
         isFetching: true,
@@ -27,6 +27,88 @@ describe('Reducers', () => {
       }, action);
       expect(state.isAuthenticated).to.eql(action.isAuthenticated);
     });
+
+    it('should handle a login failure', () => {
+      const action = {
+        type: 'LOGIN_FAILURE',
+        isFetching: true,
+        isAuthenticated: false,
+        errorMessage: 'User not found'
+      };
+
+      const state = authReducer({
+        isFetching: false,
+        isAuthenticated: false,
+        errorMessage: ''
+      }, action);
+      expect(state.isAuthenticated).to.eql(action.isAuthenticated);
+    });
+
+    it('should handle a signup success', () => {
+      const action = {
+        type: 'SIGNUP_SUCCESS',
+        isFetching: true,
+        isAuthenticated: true,
+        user: {
+          username: 'Max'
+        }
+      };
+
+      const state = authReducer({
+        user: {},
+        isFetching: false,
+        isAuthenticated: false,
+      }, action);
+      expect(state.isAuthenticated).to.eql(action.isAuthenticated);
+    });
+
+    it('should handle a signup failure', () => {
+      const action = {
+        type: 'SIGNUP_FAILURE',
+        isFetching: true,
+        isAuthenticated: false,
+        errorMessage: 'Username taken'
+      };
+
+      const state = authReducer({
+        isFetching: false,
+        isAuthenticated: false,
+        errorMessage: ''
+      }, action);
+      expect(state.isAuthenticated).to.eql(action.isAuthenticated);
+    });
+
+    it('should handle a logout success', () => {
+      const action = {
+        type: 'LOGOUT_SUCCESS',
+        isFetching: true,
+        isAuthenticated: false,
+      };
+
+      const state = authReducer({
+        user: {},
+        isFetching: false,
+        isAuthenticated: true,
+      }, action);
+      expect(state.isAuthenticated).to.eql(action.isAuthenticated);
+    });
+
+    it('should handle a logout failure', () => {
+      const action = {
+        type: 'LOGIN_FAILURE',
+        isFetching: true,
+        isAuthenticated: false,
+        errorMessage: 'Could not logout'
+      };
+
+      const state = authReducer({
+        user: {},
+        isFetching: false,
+        isAuthenticated: false,
+        errorMessage: ''
+      }, action);
+      expect(state.isAuthenticated).to.eql(action.isAuthenticated);
+    });
   });
 
   describe('listReducer', () => {
@@ -38,6 +120,36 @@ describe('Reducers', () => {
 
       const state = listReducer({userInput: ''}, action);
       expect(state.userInput).to.eql(action.inputData);
+    });
+
+    it('should update suggestions with new options', () => {
+      const action = {
+        type: 'UPDATE_SEARCH_SUGGESTIONS_SUCCESS',
+        suggestions: ['Mad Max: Fury Road', 'Mad Max']
+      };
+
+      const state = listReducer({suggestions: []}, action);
+      expect(state.suggestions).to.eql(action.suggestions);
+    });
+
+    it('should toggle check', () => {
+      const action = {
+        type: 'TOGGLE_CHECK',
+        checked: false
+      };
+
+      const state = listReducer({checked: true}, action);
+      expect(state.checked).to.eql(action.checked);
+    });
+
+    it('should update the filter', () => {
+      const action = {
+        type: 'UPDATE_FILTER_STATE',
+        filter: 'Movies'
+      };
+
+      const state = listReducer({filter: ''}, action);
+      expect(state.filter).to.eql(action.filter);
     });
   });
 
