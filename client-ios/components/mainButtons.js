2fb64c6cd23b926fe485e8a2dab6e0f431e88682
Andrew Vye <a.vye26@gmail.com>
2016-07-05 23:19:38 -0700

    (refactor) Update mainButtons to have individual items to be able to have distinct images and unique border styles

--- client-ios/components/mainButtons.js
+++ client-ios/components/mainButtons.js
@@ -19,28 +19,93 @@ export default class mainButtons extends Component {
   }
 
   render() {
-
     const adding = !(this.props.userInput.length === 0);
-    const buttons = this.props.buttons.map((button, index) => {
-      return (
-        <View key={button.icon}>
-          <TouchableOpacity
-            style={(this.props.filter === button.category && this.props.saved) ? styles.mainButtonItemAdded : styles.mainButton}
-            onPress={() => {this.props.mainButtonPressed(button.category)}}
-          >
-            <Icon
-              name={adding ? 'ios-add-outline' : button.icon}
-              size={45}
-            />
-            {(this.props.filter === button.category && this.props.saved) ? <Text style={styles.buttonCategoryText}>Added!</Text> : <Text style={styles.buttonCategoryText}>{button.category}</Text>}
-          </TouchableOpacity>
-        </View>
-      )
-    });
 
     return (
       <View style={styles.buttonContainer} >
-        {buttons}
+        <View>
+          <TouchableOpacity
+            style={(this.props.filter === 'DO' && this.props.saved) ? styles.mainButtonItemAdded : styles.mainButton}
+            onPress={() => {this.props.mainButtonPressed('DO')}}
+          >
+            <Image
+              style={adding ? 'ios-add-outline' : styles.buttonImage}
+              style={styles.buttonImage}
+              source={adding ? require('../assets/PlusMath-50.png') :require('../assets/Clipboard-50.png')}
+            />
+            {(this.props.filter === 'DO' && this.props.saved) ? <Text style={styles.buttonCategoryText}>Added!</Text> : <Text style={styles.buttonCategoryText}>DO</Text>}
+          </TouchableOpacity>
+        </View>
+
+        <View>
+          <TouchableOpacity
+            style={(this.props.filter === 'BUY' && this.props.saved) ? styles.mainButtonItemAdded : styles.mainButton}
+            onPress={() => {this.props.mainButtonPressed('BUY')}}
+          >
+            <Image
+              style={adding ? 'ios-add-outline' : styles.buttonImage}
+              style={styles.buttonImage}
+              source={adding ? require('../assets/PlusMath-50.png') :require('../assets/ShoppingCart-50.png')}
+            />
+            {(this.props.filter === 'BUY' && this.props.saved) ? <Text style={styles.buttonCategoryText}>Added!</Text> : <Text style={styles.buttonCategoryText}>BUY</Text>}
+          </TouchableOpacity>
+        </View>
+
+        <View>
+          <TouchableOpacity
+            style={(this.props.filter === 'READ' && this.props.saved) ? styles.mainButtonItemAdded : styles.mainButton}
+            onPress={() => {this.props.mainButtonPressed('READ')}}
+          >
+            <Image
+              style={adding ? 'ios-add-outline' : styles.buttonImage}
+              style={styles.buttonImage}
+              source={adding ? require('../assets/PlusMath-50.png') :require('../assets/Literature-50.png')}
+            />
+            {(this.props.filter === 'READ' && this.props.saved) ? <Text style={styles.buttonCategoryText}>Added!</Text> : <Text style={styles.buttonCategoryText}>READ</Text>}
+          </TouchableOpacity>
+        </View>
+
+        <View>
+          <TouchableOpacity
+            style={(this.props.filter === 'LISTEN' && this.props.saved) ? styles.mainButtonItemAdded : styles.mainButton}
+            onPress={() => {this.props.mainButtonPressed('LISTEN')}}
+          >
+            <Image
+              style={adding ? 'ios-add-outline' : styles.buttonImage}
+              style={styles.buttonImage}
+              source={adding ? require('../assets/PlusMath-50.png') :require('../assets/Headphones-50.png')}
+            />
+            {(this.props.filter === 'LISTEN' && this.props.saved) ? <Text style={styles.buttonCategoryText}>Added!</Text> : <Text style={styles.buttonCategoryText}>LISTEN</Text>}
+          </TouchableOpacity>
+        </View>
+
+        <View>
+          <TouchableOpacity
+            style={(this.props.filter === 'EAT' && this.props.saved) ? styles.mainButtonItemAdded : styles.mainButton}
+            onPress={() => {this.props.mainButtonPressed('EAT')}}
+          >
+            <Image
+              style={adding ? 'ios-add-outline' : styles.buttonImage}
+              style={styles.buttonImage}
+              source={adding ? require('../assets/PlusMath-50.png') :require('../assets/Restaurant-50.png')}
+            />
+            {(this.props.filter === 'EAT' && this.props.saved) ? <Text style={styles.buttonCategoryText}>Added!</Text> : <Text style={styles.buttonCategoryText}>EAT</Text>}
+          </TouchableOpacity>
+        </View>
+
+        <View>
+          <TouchableOpacity
+            style={(this.props.filter === 'WATCH' && this.props.saved) ? styles.mainButtonItemAdded : styles.mainButton}
+            onPress={() => {this.props.mainButtonPressed('WATCH')}}
+          >
+            <Image
+              style={adding ? 'ios-add-outline' : styles.buttonImage}
+              style={styles.buttonImage}
+              source={adding ? require('../assets/PlusMath-50.png') :require('../assets/Documentary-50.png')}
+            />
+            {(this.props.filter === 'WATCH' && this.props.saved) ? <Text style={styles.buttonCategoryText}>Added!</Text> : <Text style={styles.buttonCategoryText}>WATCH</Text>}
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
